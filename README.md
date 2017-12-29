e66e65cceb7e7d0cd5fb3399d97d4c305ae2bfb5
xschen <xs0040@gmail.com>
2017-12-29 19:26:33 +0800

    predicting script for the glove ffn

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ Fake news generator and detector using keras
 
 The fake news data is from [https://github.com/GeorgeMcIntire/fake_real_news_dataset](https://github.com/GeorgeMcIntire/fake_real_news_dataset)
 
+# Fake News Detector
+
+The following neural network models are implemented for fake new detector:
+
+* LSTM recurrent network with embedding layer
+    * training: run fake_news_train/lstm_classifier_train.py to train and save the model
+    * predicting: run fake_news_train/lstm_classifier_predict.py to load the trained model for prediction
+ 
+* Feed-forward network with GloVe embedding layer
+    * training: run fake_news_train/glove_ffn_classifier_train.py to train and save the model
+    * predicting: run fake_news_train/glove_ffn_classifier_predict.py to load the trained model for prediction
+    
 Below are the training history in terms of loss and accuracy for a number of neural network implemented in keras:
 
 ![lstm-history.png](/fake_news_train/reports/lstm-history.png)
---
+++ fake_news_train/glove_ffn_classifier_predict.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+
+from sklearn import metrics
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from fake_news_train.utils import plot_confusion_matrix
+from fake_news_classifiers.feedforward_networks import GloveFeedforwardNet
+import numpy as np
+
+
+def main():
+    np.random.seed(42)
+    data_dir_path = './data'
+    very_large_data_dir_path = './very_large_data'
+    model_dir_path = './models'
+    config_file_path = model_dir_path + '/' + GloveFeedforwardNet.model_name + '-config.npy'
+    weight_file_path = model_dir_path + '/' + GloveFeedforwardNet.model_name + '-weights.h5'
+
+    print('loading csv file ...')
+
+    # Import `fake_or_real_news.csv`
+    df = pd.read_csv(data_dir_path + "/fake_or_real_news.csv")
+
+    # Set `y`
+    Y = [1 if label == 'REAL' else 0 for label in df.label]
+    # Drop the `label` column
+    df.drop("label", axis=1)
+
+    X = df['text']
+
+    config = np.load(config_file_path).item()
+
+    classifier = GloveFeedforwardNet(config)
+    classifier.load_weights(weight_file_path)
+    classifier.load_glove(very_large_data_dir_path)
+
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    print('testing size: ', len(Xtest))
+
+    print('start predicting ...')
+    pred = classifier.predict(Xtest)
+    print(pred)
+    score = metrics.accuracy_score(Ytest, pred)
+    print("accuracy:   %0.3f" % score)
+    cm = metrics.confusion_matrix(Ytest, pred, labels=[0, 1])
+    plot_confusion_matrix(cm, classes=[0, 1])
+
+
+if __name__ == '__main__':
+    main()
